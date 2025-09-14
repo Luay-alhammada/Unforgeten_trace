c1e20662b082049a771acd1c7d2fa6bb7f1a87da
Luay-alhammada <alhammada.luay@gmail.com>
2025-09-14 20:18:59 +0200

    Update dash.py

--- dash.py
+++ dash.py
@@ -99,16 +99,6 @@ st.markdown("""
 
 st.divider()
 
-years = sorted(df['date_in'].dt.year.dropna().unique().astype(int), reverse=True)
-years_with_all = ['جميع السنوات'] + years
-
-selected_year = st.selectbox("اختر السنة", years_with_all, key="year_select")
-
-if selected_year == "جميع السنوات":
-    df_filtered = df.copy()
-else:
-    df_filtered = df[df['date_in'].dt.year == selected_year]
-
 col1, col2, col4, col3 = st.columns([1, 4, 1, 4])
 
 # -------------------------------
@@ -116,8 +106,14 @@ col1, col2, col4, col3 = st.columns([1, 4, 1, 4])
 # -------------------------------
 with col1:
     st.header("المرشحات")
+    years = sorted(df['date_in'].dt.year.dropna().unique().astype(int), reverse=True)
+    years_with_all = ['جميع السنوات'] + years
     selected_year = st.selectbox("اختر السنة", years_with_all)
 
+if selected_year == "جميع السنوات":
+    df_filtered = df.copy()
+else:
+    df_filtered = df[df['date_in'].dt.year == selected_year]
 
 # -------------------------------
 # Middle Column: Map
@@ -436,7 +432,4 @@ st.markdown("""
     <h5>• <b>للتواصل:</b> alhammada.luay@gmail.com</h5>
 </div>
 """, unsafe_allow_html=True)
-
-
-
 

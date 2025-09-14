75509e7035b20beefa24735b2d7ea7a716fbfb80
Luay-alhammada <alhammada.luay@gmail.com>
2025-09-14 15:49:53 +0200

    Update dash.py

--- dash.py
+++ dash.py
@@ -104,7 +104,16 @@ col1, col2, col4, col3 = st.columns([1, 4, 1, 4])
 # -------------------------------
 # Left Column: Filters
 # -------------------------------
+with col1:
+    st.header("المرشحات")
+    years = sorted(df['date_in'].dt.year.dropna().unique().astype(int), reverse=True)
+    years_with_all = ['جميع السنوات'] + years
+    selected_year = st.selectbox("اختر السنة", years_with_all)
 
+if selected_year == "جميع السنوات":
+    df_filtered = df.copy()
+else:
+    df_filtered = df[df['date_in'].dt.year == selected_year]
 
 # -------------------------------
 # Middle Column: Map
@@ -249,21 +258,6 @@ with col6:
 
 
 col9, col10, col11, col12 = st.columns([1, 4, 1, 4])
-with col9:
-    st.header("المرشحات")
-    years = sorted(df['date_in'].dt.year.dropna().unique().astype(int), reverse=True)
-    years_with_all = ['جميع السنوات'] + years
-    selected_year = st.selectbox(
-    "اختر السنة", 
-    years_with_all, 
-    key="year_selectbox"
-)
-
-if selected_year == "جميع السنوات":
-    df_filtered = df.copy()
-else:
-    df_filtered = df[df['date_in'].dt.year == selected_year]
-
 
 with col10:
     st.markdown("<h4>5 - أماكن الاعتقال في الجوية</h4>", unsafe_allow_html=True)
@@ -433,7 +427,4 @@ st.markdown("""
     <h5>• <b>للتواصل:</b> alhammada.luay@gmail.com</h5>
 </div>
 """, unsafe_allow_html=True)
-
-
-
 

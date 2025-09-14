1d535609dddd22688229b77fc2286a82fedc8d93
Luay-alhammada <alhammada.luay@gmail.com>
2025-09-14 14:27:56 +0200

    Update dash.py

--- dash.py
+++ dash.py
@@ -250,16 +250,7 @@ with col6:
 
 col9, col10, col11, col12 = st.columns([1, 4, 1, 4])
 
-with col9:
-    st.header("المرشحات")
-    years = sorted(df['date_in'].dt.year.dropna().unique().astype(int), reverse=True)
-    years_with_all = ['جميع السنوات'] + years
-    selected_year = st.selectbox("اختر السنة", years_with_all)
 
-if selected_year == "جميع السنوات":
-    df_filtered = df.copy()
-else:
-    df_filtered = df[df['date_in'].dt.year == selected_year]
 with col10:
     st.markdown("<h4>5 - أماكن الاعتقال في الجوية</h4>", unsafe_allow_html=True)
     st.markdown(
@@ -428,4 +419,5 @@ st.markdown("""
     <h5>• <b>للتواصل:</b> alhammada.luay@gmail.com</h5>
 </div>
 """, unsafe_allow_html=True)
+
 
